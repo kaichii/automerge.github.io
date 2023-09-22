154a9284a52c19844dfe289081c7418dc64de353
alexjg <alexjg@users.noreply.github.com>
2023-09-22 16:17:18 +0000

    deploy: 7810bd3536668e8cac0990f5fd8c19c86536bd4a

--- assets/js/814f3328.04102a3d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[2535],{5641:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Automerge 2.0","permalink":"/blog/automerge-2"},{"title":"Welcome","permalink":"/blog/welcome"}]}')}}]);
